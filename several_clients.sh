c59bb5c5894d6e91279c1a224c3d82e1d0f7021b
Alexandre Dantas <alex.dantas92@gmail.com>
2012-02-01 15:18:38 -0200

    Poucas Mudancas

--- several_clients.sh
+++ several_clients.sh
@@ -9,6 +9,6 @@ while [ $i -lt $1 ]
 do
 	echo "Iniciando cliente $i..."
 #	gnome-terminal --tab -e "/bin/bash -c 'wget alexdantas:27015/$2; exec /bin/bash -i'"
-	gnome-terminal --tab -e "wget alexdantas:27015/$2"
+	gnome-terminal --tab -e "wget 127.0.0.1:27015/$2 && read a"
 	i=$(( $i + 1 ))
 done
